22a149ae7410f2b8d5ceaa626815f77035f1a5f9
ajh1138 <ajh1138@gmail.com>
2024-03-30 19:30:59 -0500

    Add project files.

---
+++ GitRepositoryWindowForeverPackage.cs
@@ -0,0 +1,62 @@
+﻿global using System;
+global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using Task = System.Threading.Tasks.Task;
+using System.Runtime.InteropServices;
+using System.Threading;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Tostadasoft.GitRepositoryWindowForever
+{
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	[ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+	[ProvideOptionPage(typeof(OptionsProvider.MyExtensionOptionsOptions), "GitRepositoryWindowForever", "MyExtensionOptions", 0, 0, true, SupportsProfiles = true)]
+	[ProvideProfile(typeof(OptionsProvider.MyExtensionOptionsOptions), "GitRepositoryWindowForever", "MyExtensionOptions", 0, 0, true)]
+	[Guid(PackageGuids.GitRepositoryWindowForeverString)]
+	public sealed class GitRepositoryWindowForeverPackage : ToolkitPackage
+	{
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+
+			if (MyExtensionOptions.Instance != null)
+			{
+				if(MyExtensionOptions.Instance.ShowGitRepoWindowOnStartup)
+				{
+					await OpenGitWindowAsync();
+				}
+
+				if (MyExtensionOptions.Instance.ShowGitRepoWindowOnSolutionOpen)
+				{
+					Microsoft.VisualStudio.Shell.Events.SolutionEvents.OnAfterOpenSolution += this.OnAfterOpenSolution;
+				}
+
+				if (MyExtensionOptions.Instance.ShowGitRepoWindowOnDebugStart)
+				{
+					KnownUIContexts.DebuggingContext.WhenActivated(() =>
+					{
+						_ = OpenGitWindowAsync();
+					});
+				}
+			}
+		}
+
+		public async Task OpenGitWindowAsync()
+		{
+			await this.JoinableTaskFactory.SwitchToMainThreadAsync(default);
+
+			DTE dte = AsyncPackage.GetGlobalService(typeof(DTE)) as DTE;
+
+			dte.ExecuteCommand("View.GitRepositoryWindow");
+		}
+
+		public void OnAfterOpenSolution(object sender = null, EventArgs e = null)
+		{
+			_ = OpenGitWindowAsync();
+		}
+	}
+}
---
+++ Options/MyExtensionOptions.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Tostadasoft.GitRepositoryWindowForever
+{
+	internal partial class OptionsProvider
+	{
+		// Register the options with this attribute on your package class:
+		// [ProvideOptionPage(typeof(OptionsProvider.MyExtensionOptionsOptions), "Tostadasoft.GitRepositoryWindowForever", "MyExtensionOptions", 0, 0, true, SupportsProfiles = true)]
+		[ComVisible(true)]
+		public class MyExtensionOptionsOptions : BaseOptionPage<MyExtensionOptions> { }
+	}
+
+	public class MyExtensionOptions : BaseOptionModel<MyExtensionOptions>
+	{
+		[Category("General")]
+		[DisplayName("Always show on Visual Studio startup")]
+		[Description("Note: Setting to False will not close the Git Repo window.")]
+		[DefaultValue(true)]
+		public bool ShowGitRepoWindowOnStartup { get; set; } = true;
+
+		[Category("General")]
+		[DisplayName("Show on Debug start")]
+		[Description("Note: Setting to False will not close the Git Repo window on debug.")]
+		[DefaultValue(true)]
+		public bool ShowGitRepoWindowOnDebugStart { get; set; } = true;
+
+		[Category("General")]
+		[DisplayName("Always show after opening a Solution")]
+		[Description("Note: Setting to False will not close the Git Repo window when a solution is loaded.")]
+		[DefaultValue(true)]
+		public bool ShowGitRepoWindowOnSolutionOpen { get; set; } = true;
+	}
+}
